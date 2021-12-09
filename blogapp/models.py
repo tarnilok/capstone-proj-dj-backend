216d4d680025c91adac1fe62781ab1b528f7eadc
tarnilok <tarnilok@gmail.com>
2021-12-10 00:42:27 +0300

    added user field to models

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import NullBooleanField
 
 class Card(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=100, unique=True)
     image_url = models.URLField()
     content = models.TextField()
